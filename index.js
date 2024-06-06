7b2b73b125661faf1893f5e7e6e2ced9c8930893
gaurav parsar <98256253+INDIANgaurav@users.noreply.github.com>
2024-06-06 17:44:58 +0530

    second commit :- responsiveness and images update

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('.category-btn');
     const contents = document.querySelectorAll('.category-content');
-
+console.log(contents)
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             buttons.forEach(btn => btn.classList.remove('active'));
             button.classList.add('active');
+            console.log("button -> " ,button , "buttons ->" ,buttons)
 
             const category = button.getAttribute('data-category');
+            console.log(category)
             contents.forEach(content => {
                 content.classList.remove('active');
                 if (content.getAttribute('data-category') === category) {
